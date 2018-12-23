354c9b909a6fbf38233b8b5264e379f7aa43c3ff
mepcovenkat97 <mepcovenkat97@gmail.com>
2018-12-23 12:47:33 +0530

    SVR changed with Feature SCaling and Prediction

--- SVR_OWN.py
+++ SVR_OWN.py
@@ -7,15 +7,27 @@ dataset = pd.read_csv("Position_Salaries.csv");
 x = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
+#Splitting 
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size =0.2,random_state = 0)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+x_train = sc_X.fit_transform(x_train)
+y_train = sc_X.transform(x_test)
+sc_Y = StandardScaler()
+y_train = sc_Y.fit_transform(y_train)
+y_train = sc_Y.transform(y_test)
 
 #Fitting into SVR
 from sklearn.svm import SVR
 #'rbf' for non linear also use 'poly'
-regressor = SVR(kernal='rbf')
+regressor = SVR(gamma='scale', C=1.0, epsilon=0.2)
 regressor.fit(x,y)
 
 #Predicting new results
-y_predict = regressor.predict(6.5)
+y_predict = sc_Y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
 
 #Visualizing the SVR results
 plt.scatter(x,y,color = 'red')
